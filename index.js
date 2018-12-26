5c4b1740e0e8849769aff7d8717226a08005930d
ADE AHMID <adewale50@yahoo.co.uk>
2018-12-26 21:44:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = "39"
-var message = ` is ${less than 40} inches tall`
+var message =  "is less than 40 inches tall"
 
 module.exports = {name, height, message}
